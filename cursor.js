0c960bfb8a29a6dba7ad98281125db31b5784e38
Paolo <paolodellorti@gmail.com>
2021-08-09 17:50:08 +0100

    progetto finito

--- cursor.js
+++ cursor.js
@@ -3,13 +3,13 @@ let ball = cursorTag.querySelector("div")
 let cursorMessage = cursorTag.querySelector("div span")
 let target = document.querySelectorAll("[data-hover]")
 
-
+//assegno al div ball le coordinate del mouse
 document.addEventListener("mousemove", function(event) {
   ball.style.left = event.pageX + "px"
   ball.style.top = event.pageY + "px"
 })
 
-
+//aggiungo e rimuovo la scritta di informazioni al passaggio del mouse sugli elementi
 target.forEach(element => {
   element.addEventListener("mouseover", ()=> {
     cursorMessage.classList.add("visible")
--- script.js
+++ script.js
@@ -6,11 +6,6 @@ let recordMax = document.querySelector(".recordMax")
 let recordMin = document.querySelector(".recordMin")
 let max = document.querySelector(".max")
 let min = document.querySelector(".min")
-let m1 = document.querySelector("#m1")
-let m2 = document.querySelector("#m2")
-let m3 = document.querySelector("#m3")
-let m4 = document.querySelector("#m4")
-let m5 = document.querySelector("#m5")
 let memos = document.querySelectorAll(".memo")
 
 let display = 0
@@ -20,6 +15,7 @@ let displayMin = 0
 //chiamo subito la funzione per mostrare lo 0 appena caricata la pagina
 updateDisplay()
 
+//creo gli eventi per aggiungere e sottrarre 1 con un click e 10 con due click
 add.addEventListener('click', ()=>{
   ++display
   updateDisplay()
@@ -40,6 +36,7 @@ subtract.addEventListener('dblclick', ()=>{
   updateDisplay()
 })
 
+//creo tasto reset (R)
 document.addEventListener('keydown', function(event) {
   if (event.code == 'KeyR') {
     display = 0
@@ -48,22 +45,26 @@ document.addEventListener('keydown', function(event) {
     max.innerHTML = ""
     displayMin = 0
     min.innerHTML = ""
-    //da aggiungere reset memos
+    memos.forEach(m => {
+        resetMemo(m)
+        m.style.color = "#404040"
+    })
   }
 })
 
+//creo i reset dei singoli elementi con doppio click
 counterContainer.addEventListener('dblclick', ()=>{
   display = 0
   counter.innerHTML = display
 })
 
 recordMax.addEventListener('dblclick', ()=>{
-  displayMax = 0
+  displayMax = -1000
   max.innerHTML = ""
 })
 
 recordMin.addEventListener('dblclick', ()=>{
-  displayMin = 0
+  displayMin = 1000
   min.innerHTML = ""
 })
 
@@ -71,14 +72,15 @@ memos.forEach(m => {
   m.addEventListener('click', ()=>{
     memo(m)
   })
-});
+})
 
 memos.forEach(m => {
   m.addEventListener('dblclick', ()=>{
     resetMemo(m)
   })
-});
+})
 
+//risolvo un problema dello sfondo delle singole celle di storaggio
 memos.forEach(m => {
   m.addEventListener('mouseover', ()=>{
     if (m.innerHTML == "S" || m.innerHTML == "T" || m.innerHTML == "O" || m.innerHTML == "R" || m.innerHTML == "E") {
@@ -93,37 +95,31 @@ memos.forEach(m => {
   })
 });
 
-//tolgo la selezione del testo
+//tolgo la possibilità di selezione del testo
 window.addEventListener('selectstart', function(e){ e.preventDefault() })
 
-
+//funzione principale che aggiorna il counter
 function updateDisplay() {
   counter.innerHTML = display
 
   if (displayMax < display) {
     displayMax = display
     max.innerHTML = displayMax
-    if (min.innerHTML == ""){
-      displayMin = 0
-      min.innerHTML = 0
     }
-  }
 
   if (displayMin > display) {
     displayMin = display
     min.innerHTML = displayMin
-    if (max.innerHTML == ""){
-      displayMin = 0
-      max.innerHTML = 0
-    }
   }
 }
 
+//funzione che memorizza il numero attuale
 function memo(m) {
   m.style.color = "#f3f1e9"
   m.innerHTML = display
 }
 
+//funzione che resetta la singola cella di memoria, riportandola allo stato originale
 function resetMemo(m) {
   m.style.color = "#363636"
 
